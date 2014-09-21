77c6c8620dd1060c577d690325cd672a57c36583
Marco Sampellegrini <babbonatale@alpacaaa.net>
2014-09-21 09:13:08 +0200

    Letz do it.

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+$(document).ready(function() {
+  var html = $('html'),
+    hbody  = $('html,body'),
+    keywords_list = $('.keywords-list');
+
+  var scrollUp = function() {
+    hbody.animate({ scrollTop: 0 }, 'slow');
+  };
+
+
+  setTimeout(function() {
+    html.addClass('home-transition-in');
+  }, 100);
+
+  var genders = $('.gender');
+  genders.click(function() {
+    var g = $(this).data('type');
+    html
+      .removeClass('woman-selected man-selected')
+      .addClass(g + '-selected');
+  });
+
+  $('.city').click(function() {
+    html.removeClass('show-home home-transition-in');
+    scrollUp();
+
+    setTimeout(function() {
+      html.addClass('show-list');
+    }, 1300);
+
+    setTimeout(function() {
+      html.addClass('list-transition-in');
+    }, 1500);
+
+    var city = $(this).find('h3').text().toLowerCase();
+      city = '#' + city.replace(' ', '-');
+    keywords_list.find('li[data-type="city"]').text(city);
+  });
+
+
+  $('.list-step .keyword-detail .item-detail').click(function() {
+    html.addClass('list-transition-out');
+    scrollUp();
+
+    setTimeout(function() {
+      html.removeClass('show-list list-transition-in list-transition-out')
+        .addClass('show-detail');
+    }, 1000);
+
+    setTimeout(function() {
+      html.addClass('detail-transition-in');
+    }, 1100);
+  });
+});
